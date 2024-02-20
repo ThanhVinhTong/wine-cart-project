bc9b8f9fcd951b4cfe737e5a8a4ff994ef79c49f
ThanhVinhTong <vinhtongthanh57@gmail.com>
2024-02-20 18:32:40 +0800

    Create notification function

---
+++ wine-scrapper-model/notification/GeneralNotification.py
@@ -0,0 +1,3 @@
+# general notification
+def notify(process="unknown", message="success"):
+    print("Notification from process \"{}\" which results in \"{}\"".format(process, message))
---
+++ wine-scrapper-model/notification/SaveImageNotification.py
@@ -0,0 +1,3 @@
+# successfully save iamge notification
+def save_image_notify(image_filename, file_path):
+    print(f"Image '{image_filename}.jpg' saved successfully at {file_path}")
