60fe2cccd60b2cff18d1eabef8e7ff0dbe59b3bc
ThanhVinhTong <vinhtongthanh57@gmail.com>
2026-02-24 03:46:32 +0800

    fix: remove export authConfig

--- wine-cart/app/api/auth/[...nextauth]/route.ts
+++ wine-cart/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider  from 'next-auth/providers/google'
 
-export const authConfig = {
+const authConfig = {
   providers: [
     GoogleProvider ({
       clientId: process.env.GOOGLE_ID!,
